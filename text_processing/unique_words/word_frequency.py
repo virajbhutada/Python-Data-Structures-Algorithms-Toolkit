1b6530540348bd02c3848ed3cdc9e601b5f0f844
Viraj Bhutada <virajnbhutada24@gmail.com>
2024-09-28 11:02:09 +0530

    Delete text_processing directory

--- text_processing/unique_words/word_frequency.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-def word_frequency(text):
-    words = text.split()
-    return dict(Counter(words))
-
-# Test
-text = "hello world hello"
-print(word_frequency(text))  # Output: {'hello': 2, 'world': 1}
